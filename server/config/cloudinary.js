dcc0a15f2fbdf0de107fba346264fca20684b926
Hemanth Tejavath <tejavathhemanth41@gmail.com>
2026-04-21 22:33:33 +0530

    Fixed at 22:33

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dnbrklkrz",
-  api_key: "718883433694382",
-  api_secret: "wB9i7gNMlIthlY3oFSK6nfB8894",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 export default cloudinary;
